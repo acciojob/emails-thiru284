cb9abaa3505a5d9a561aae423bd779be3d33bb39
Thirumoorthy Velumani <thirumoorthy@mallow-tech.com>
2023-02-26 13:35:28 +0530

    Initial commit

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -25,5 +25,47 @@ public class Email {
         // 3. It contains at least one lowercase letter
         // 4. It contains at least one digit
         // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
+
+        if(oldPassword.equals(password)){
+            if(isValid(newPassword)){
+                this.password = newPassword;
+                System.out.println("Password changed successfully!");
+            }
+            else{
+                System.out.println("The new password is not valid!");
+            }
+        }
+        else{
+            System.out.println("The given password doesn't match current password.");
+        }
+    }
+
+    private boolean isValid(String password){
+        Boolean caplet = false;
+        Boolean smalet = false;
+        Boolean digit = false;
+        Boolean spchar = false;
+
+        if(password.length() < 8){
+            return false;
+        }
+
+        for(int i = 0; i < password.length(); i++){
+            char ch = password.charAt(i);
+            if(ch >= 'a' && ch <= 'z'){
+                smalet = true;
+            } else if (ch >= 'A' && ch <= 'Z') {
+                caplet = true;
+            } else if (ch >= '0' && ch <= '9') {
+                digit = true;
+            } else {
+                spchar = true;
+            }
+        }
+
+        if(caplet && smalet && digit && spchar){
+            return true;
+        }
+        return false;
     }
 }
